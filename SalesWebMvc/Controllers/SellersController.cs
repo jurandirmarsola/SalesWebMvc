e5a93688dce6d6daaabee6462161be1657feefe4
jurandirmarsola <jurandirmarsola@gmail.com>
2022-03-30 17:41:56 -0300

    Seller details and eager loading

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -64,5 +64,17 @@ namespace SalesWebMvc.Controllers
 
             return RedirectToAction(nameof(Index));
         }
+
+        public IActionResult Details(int? id)  //? indica opcional
+        {
+            if (id == null)
+                return NotFound();
+
+            var obj = _sellerService.FindById(id.Value);
+            if (obj == null)
+                return NotFound();
+
+            return View(obj);
+        }
     }
 }
