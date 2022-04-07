0d106e010dbd3154d446f5120952109f57534666
jurandirmarsola <jurandirmarsola@gmail.com>
2022-04-07 14:48:17 -0300

    Implementing simple Search

--- SalesWebMvc/Models/SalesRecord.cs
+++ SalesWebMvc/Models/SalesRecord.cs
@@ -1,12 +1,15 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace SalesWebMvc.Models
 {
     public class SalesRecord
     {
         public int Id { get; set; }
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime Date { get; set; }
         public double Amount { get; set; }
+        [Display(Name ="Status")]
         public SaleStatus SaleStatus { get; set; }
         public Seller Seller { get; set; }
 
